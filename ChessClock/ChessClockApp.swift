0cc9578a22d5c7843426728d1d4756f52c430f07
Collin Sansom <hi@collinsansom.com>
2025-01-09 01:15:26 -0600

    Initial Commit

---
+++ ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Collin Sansom on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
